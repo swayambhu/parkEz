95df5aec5d2921311553f8a619121cce0ba7499c
Swayambhu <swayambhu43@gmail.com>
2023-02-15 10:17:19 -0500

    basic layout and documents added

--- frontend/src/layouts/Header.js
+++ frontend/src/layouts/Header.js
@@ -1,9 +1,22 @@
+import styled from 'styled-components'
+
 const Header = () => {
     return(
-        <header>
-            Header
-        </header>
+        <HeaderWrapper>
+            
+        </HeaderWrapper>
     )
 }
+
+const HeaderWrapper = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px 40px;
+    box-shadow:0px 0px 12px 4px #cccccc;
+    position: sticky;
+    width: 100vw;
+`
+
 
 export default Header
