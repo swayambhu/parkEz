80422506f4d68f268c32c1e52f4d05678be88933
Swayambhu <swayambhu43@gmail.com>
2023-03-07 00:58:29 -0500

    home page done

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import Brands from "../components/Pages/Home Page/OurPartners"
 import HeroSection from "../components/Pages/Home Page/HeroSection"
 import KeyProperties from "../components/Pages/Home Page/KeyProperties"
 import Services from "../components/Pages/Home Page/Services"
+import Reviews from "../components/Pages/Home Page/Reviews"
 
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <Services/>
             <KeyProperties />
             <Brands/>
+            <Reviews />
         </>
     )
 }
