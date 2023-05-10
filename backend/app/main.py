9676939e92322a16ea01cf2287a89bd566240e19
Swayambhu <swayambhu43@gmail.com>
2023-05-09 23:22:46 -0400

    login page

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,9 +4,18 @@ from app.database.database import engine
 from app.routers.Authentication import authentication
 from app.routers.Business import business
 from app.routers import services
+from fastapi.middleware.cors import CORSMiddleware
 
 Models.Base.metadata.create_all(bind= engine)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(authentication.router)
 app.include_router(business.router)
