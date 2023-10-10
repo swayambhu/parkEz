0c7b58f909dc8caba0554d2598fd0dbace42787f
Tom C <78508475+tc10815@users.noreply.github.com>
2023-10-10 14:27:46 -0400

    Jenkins demonstration

--- frontend/src/components/Pages/Home Page/HeroSection.js
+++ frontend/src/components/Pages/Home Page/HeroSection.js
@@ -6,7 +6,7 @@ const HeroSection = () => {
     return(
         <Section image={require("../../../assets/images/Home Page/Home-Hero-Section.png")}>
             <div>
-                <h1>Automated Parking Lot Management Made Easy</h1>
+                <h1>Automated Parking Lot Management Made Easy!</h1>
                 <p>
                     Increase revenue for your business by using AI to find customers parking spaces and catch and tow non-patron overparkers illegally using your parking lot.
 
