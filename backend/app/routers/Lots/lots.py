98d46face4405abbd2cf300b99629d90cac0626c
Tom C <78508475+tc10815@users.noreply.github.com>
2023-10-19 19:22:05 -0400

    Machine learning models now categorize spaces as occupied or unoccupied and save this data in camimage table with each image. Haven't studied how good the ML is (very small dataset) and no visual representation of results, but machine learning is now being done in ParkEz.

--- backend/app/routers/Lots/lots.py
+++ backend/app/routers/Lots/lots.py
@@ -2,6 +2,8 @@ import os, json, torch, shutil
 from PIL import Image
 from sqlalchemy.orm import Session
 from fastapi.templating import Jinja2Templates
+from torch import nn, optim
+import torchvision.transforms as transforms
 from fastapi import APIRouter, HTTPException, UploadFile, File, Depends
 from app.database.database import get_db
 from app.database.Models.Models import LotMetadata, CamImage
@@ -61,7 +63,27 @@ async def upload_image(
     for spot in spots_data.keys():
         x, x_w, y, y_h = spots_data[spot]
         cropped_image = pil_image.crop((x, y, x_w, y_h))
-        # Future home of ml code
+
+        # Convert cropped image of spot to form usable by ML model using transform defined elsewhere
+        input_tensor = transform(cropped_image)
+        input_tensor = input_tensor.unsqueeze(0)  # Add a batch dimension
+
+        model = CNN()  # Instantiate your model
+        model_path = os.path.join('app', 'lots', camera_name, 'models', spot + '.pth')
+
+        # Depending on the environment you're running this code in, choose the appropriate loading method
+        # model_state_dict = torch.load(model_path, map_location=torch.device('cpu'))
+        model.load_state_dict(torch.load(model_path)) 
+        model.eval()  # Set the model to evaluation mode
+
+        with torch.no_grad():
+            output = model(input_tensor)
+            _, predicted = torch.max(output, 1)
+
+        # Access the prediction result
+        prediction = predicted.item()
+        if prediction == 0: 
+            labels[spot] = True
 
     lot_image.human_labels = json.dumps(labels)
     lot_image.model_labels = json.dumps(labels)
@@ -75,3 +97,99 @@ async def upload_image(
         human_labels=lot_image.human_labels,
         model_labels=lot_image.model_labels
     )
+
+# CNN model good at determining if car in spot, from notebook, will separate to another file eventually for organization
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        
+        # Convolutional layer 1
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)  
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu1 = nn.ReLU()
+
+        # Convolutional layer 2
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.relu2 = nn.ReLU()
+        
+        # Convolutional layer 3
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.relu3 = nn.ReLU()
+
+        # Convolutional layer 4
+        self.conv4 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(512)
+        self.relu4 = nn.ReLU()
+
+        # Convolutional layer 5
+        self.conv5 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(512)
+        self.relu5 = nn.ReLU()
+
+        # Max pool layer
+        self.pool = nn.MaxPool2d(kernel_size=2)
+
+        # Dropout layer
+        self.dropout = nn.Dropout(p=0.5)
+
+        # Fully connected layers
+        self.fc1 = nn.Linear(512 * 8 * 8, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 2)
+
+    def forward(self, x):
+        # Convolutional layer 1
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
+        out = self.pool(out)
+
+        # Convolutional layer 2
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu2(out)
+        out = self.pool(out)
+
+        # Convolutional layer 3
+        out = self.conv3(out)
+        out = self.bn3(out)
+        out = self.relu3(out)
+        out = self.pool(out)
+
+        # Convolutional layer 4
+        out = self.conv4(out)
+        out = self.bn4(out)
+        out = self.relu4(out)
+        out = self.pool(out)
+
+        # Convolutional layer 5
+        out = self.conv5(out)
+        out = self.bn5(out)
+        out = self.relu5(out)
+        out = self.pool(out)
+
+        # Flatten for fully connected layer
+        out = out.view(out.size(0), -1)
+
+        # Fully connected layer 1
+        out = self.fc1(out)
+        out = self.dropout(out)
+
+        # Fully connected layer 2
+        out = self.fc2(out)
+        out = self.dropout(out)
+
+        # Fully connected layer 3
+        out = self.fc3(out)
+
+        return out
+
+
+# Originally in Model_Maker notebook, this preps cropped parking spaces for ML processing
+transform = transforms.Compose([
+    transforms.Resize((256, 256)),  # Resize to 256x256
+    transforms.ToTensor(),  # Convert to PyTorch tensor
+    transforms.Normalize((0.5,0.5,0.5,), (0.5,0.5,0.5,))  # Normalize pixel values in the range [-1, 1]
+])
